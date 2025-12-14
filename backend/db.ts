0f2c8deb3f49e9b463bc8833d9bb184805d203d0
abhilashtyagiii <abhilash.t@esolglobal.com>
2025-12-14 14:02:13 +0530

    Delete backend directory

--- backend/db.ts
+++
@@ -1,3 +0,0 @@
-// MongoDB connection is now handled in mongodb.ts
-// This file is kept for compatibility
-export { connectToMongoDB as connectDB } from './mongodb';
--- backend/models/index.ts
+++
@@ -1,6 +0,0 @@
-export { UserModel, IUser } from './User';
-export { CandidateModel, ICandidate } from './Candidate';
-export { InterviewModel, IInterview } from './Interview';
-export { AnalysisReportModel, IAnalysisReport } from './AnalysisReport';
-export { RecruiterMetricsModel, IRecruiterMetrics } from './RecruiterMetrics';
-export { PipelineMonitoringModel, IPipelineMonitoring } from './PipelineMonitoring';
